fdb8b3752cec263ac432f8fb45378068079b33eb
gustavovaznet <98855318+gustavovaznet@users.noreply.github.com>
2022-02-08 17:55:01 -0300

    Delete modules files

--- modules/customers/employees/login_check.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//SESSION CHECK
-	if(!$_SESSION['user']){
-		header('location: ../../index.php?log=invalid');
-	}
-?>
--- modules/customers/employees/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//LOGOUT
-	session_start();
-	session_destroy();
-	header('location: ../../index.php');
-	exit();
-?>
